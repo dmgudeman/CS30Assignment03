1df4587ba225d9876f91395090b279d2c8af3508
David Gudeman <davidmgudeman@gmail.com>
2015-05-22 16:45:10 -0700

    very good working copy

--- CS30Assignment03.sh
+++ CS30Assignment03.sh
@@ -11,13 +11,24 @@ if [ $remindersNum -eq 0 ]; then
 
 elif [ $remindersNum -lt 11 ]; then
    let i=0
-   while [ $i -lt $remindersNum ]
-   do
-      echo please enter a date
-      read agendaDate
+   while [ $i -lt $remindersNum ]; do
+    echo please enter a date, first:
+    while [[ ! ${get_month} =~ ^[0-9]+$ ]]; do
+        echo ""
+        echo "enter a number for the MONTH (1-12):"
+        read get_month
+        ! [[ ${get_month} -ge 1 && ${get_month} -le 12  ]] && unset get_month
+    done
+    
+    while [[ ! ${get_day} =~ ^[0-9]+$ ]]; do                                   
+          echo "enter a number for the DAY (1-31):"                    
+          read get_day
+           ! [[ ${get_day} -ge 1 && ${get_day} -le 31  ]] && unset get_day    
+      done  
+
       echo please enter a description of the event
       read agendaDescription
-      echo -e $agendaDate ' \t ' $agendaDescription >> ~/agenda
+      echo -e $get_month/$get_day ' \t ' $agendaDescription >> ./agenda
       let i=$i+1
    done
 
