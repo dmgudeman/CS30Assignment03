5855a3855f11ca1acf70815bca5fbf47ccc0dbba
David Gudeman <davidmgudeman@gmail.com>
2015-05-22 17:19:59 -0700

    completed date book assignment

--- CS30Assignment03.sh
+++ CS30Assignment03.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 
-echo ""
-echo ""
-let remindersNum=0
-read -p "How many reminders would you like to enter? 
-Type 0 to quit" remindersNum
+let i=0
+while [[ ! ${remindersNum} =~ ^[0-9]+$ ]]; do
+   read -p "How many reminders would you like to enter? 
+   ***type 0 to quit" remindersNum
+   ! [[ ${remindersNum} -ge 1 && ${remindersNum} -le 12  ]] && unset remindersNum
+     if [ $remindersNum -eq 0 ]; then
+        exit
+     fi
+done
 
-if [ $remindersNum -eq 0 ]; then
-   exit
-
-elif [ $remindersNum -lt 11 ]; then
-   let i=0
-   while [ $i -lt $remindersNum ]; do
-    echo please enter a date, first:
-    while [[ ! ${get_month} =~ ^[0-9]+$ ]]; do
-        echo ""
-        echo "enter a number for the MONTH (1-12):"
-        read get_month
-        ! [[ ${get_month} -ge 1 && ${get_month} -le 12  ]] && unset get_month
-    done
-    
-    while [[ ! ${get_day} =~ ^[0-9]+$ ]]; do                                   
-          echo "enter a number for the DAY (1-31):"                    
-          read get_day
-           ! [[ ${get_day} -ge 1 && ${get_day} -le 31  ]] && unset get_day    
-      done  
-
-      echo please enter a description of the event
-      read agendaDescription
-      echo -e $get_month/$get_day ' \t ' $agendaDescription >> ./agenda
-      let i=$i+1
+while [ $i -lt $remindersNum ]; do
+   unset get_month
+   unset get_day
+   echo please enter a date, first:
+   while [[ ! ${get_month} =~ ^[0-9]+$ ]]; do
+      echo "enter a number for the MONTH (1-12):"
+      read get_month
+      ! [[ ${get_month} -ge 1 && ${get_month} -le 12  ]] && unset get_month
    done
+    
+   while [[ ! ${get_day} =~ ^[0-9]+$ ]]; do                                   
+      echo "enter a number for the DAY (1-31):"                    
+      read get_day
+      ! [[ ${get_day} -ge 1 && ${get_day} -le 31  ]] && unset get_day    
+   done  
 
-else
-   echo please enter an integer less than 11 
-   ./$0
+   echo please enter a description of the event
+   read agendaDescription
+   echo -e $get_month/$get_day ' \t ' $agendaDescription >> ./agenda
+   let i=$i+1
+done
 
-fi 2>/dev/null
+
+2>/dev/null
 
